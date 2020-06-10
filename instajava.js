36fb1beb6b381390193fbb79909748f84ec14fda
mushroommunk <66632856+mushroommunk@users.noreply.github.com>
2020-06-10 11:28:52 -0400

    Update instajava.js

--- instajava.js
+++ instajava.js
@@ -3,6 +3,8 @@ var APP_SECRET = "cffa04e907fb72a11a9786f31cda30a8";
 var APP_REDIRECT_URI = "https://mushroommunk.github.io/";
 var AUTH_WINDOW_URL = "https://api.instagram.com/oauth/authorize?client_id=3380281275315253&redirect_uri=https://mushroommunk.github.io/&scope=user_profile,user_media&response_type=code";
 var AUTH_CODE = "";
+var AUTH_TOKEN = "";
+var USER_ID = "";
 
 function load(){
 
@@ -39,5 +41,4 @@ function getToken(){
         method: 'POST',
         body: formData
     }).then(response => response.json()).then(json => console.log(json));
-    console.log(json);
 }
